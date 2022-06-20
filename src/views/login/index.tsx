01758a7ab2d5bfc6fc01aee89e7afc89e936746b
marcolongitude <marcocpdti@gmail.com>
2022-06-19 23:21:57 -0300

    add component test login

---
+++ src/views/login/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// import { Container } from './styles';
+
+const login: React.FC = () => {
+  return (
+    <View>
+      <Text>Login</Text>
+    </View>
+  );
+};
+
+export default login;
